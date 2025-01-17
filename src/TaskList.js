ee2c87f6041a6a4718b9a3dfea64f55afe7cea75
Rupendra Singh <singhrupendra1907@gmail.com>
2025-01-17 12:49:15 +0530

    add sort option for sorting of tasks

--- src/TaskList.js
+++ src/TaskList.js
@@ -3,25 +3,23 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function TaskList({ tasks, updateTask, deleteTask }) {
-  return (
-    <ul>
-      {tasks.map((task) => (
-        <li key={task.id} style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-          <input type="checkbox" defaultChecked={task?.completed} value={task?.completed} onClick={() => updateTask({ ...task, completed: !task.completed })} />
-            {/* {task.completed ? "Mark Incomplete" : "Mark Complete"} */}
-          {/* </button> */}
-          <Link to={`/task/${task.id}`}><span>{task.title}</span></Link>
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <ul>
+            {tasks.map((task) => (
+                <li key={task.id} style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+                    <input type="checkbox" defaultChecked={task?.completed} value={task?.completed} onClick={() => updateTask({ ...task, completed: !task.completed })} />
+                    <Link to={`/task/${task.id}`}><span>{task.title}</span></Link>
+                    <button onClick={() => deleteTask(task.id)}>Delete</button>
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 TaskList.propTypes = {
     tasks: PropTypes.array.isRequired,
     updateTask: PropTypes.func.isRequired,
     deleteTask: PropTypes.func.isRequired,
-  };
+};
 
 export default TaskList;
--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -3,59 +3,89 @@ import TaskList from "../TaskList";
 import TaskForm from "../TaskForm";
 
 function HomePage() {
-  const initialValue = localStorage.getItem('data') !== null ? JSON.parse(localStorage.getItem('data')): [];  
-  const [tasks, setTasks] = useState(initialValue);
-  const [filteredTask, setFilteredTask] = useState(tasks);
-  const [filterValue, setFilterValue] = useState('');
+    const initialValue = localStorage.getItem('data') !== null ? JSON.parse(localStorage.getItem('data')) : [];
+    const [tasks, setTasks] = useState(initialValue);
+    const [filteredTask, setFilteredTask] = useState(tasks);
+    const [filterValue, setFilterValue] = useState('');
+    const [sortType, setSortType] = useState('ASC');
 
-  // Add a new task
-  const addTask = (task) => {
-    setTasks([...tasks, { ...task, id: Date.now(), createAt: new Date().toLocaleString() }]);
-  };
+    // Add a new task
+    const addTask = (task) => {
+        setTasks([...tasks, { ...task, id: Date.now(), createAt: new Date().toLocaleString() }]);
+    };
 
-  // Update an existing task
-  const updateTask = (updatedTask) => {
-    setTasks(tasks.map((task) => (task.id === updatedTask.id ? updatedTask : task)));
-  };
+    // Update an existing task
+    const updateTask = (updatedTask) => {
+        setTasks(tasks.map((task) => (task.id === updatedTask.id ? updatedTask : task)));
+    };
 
-  // Delete a task
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
+    // Delete a task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
 
-  const setFilteredValue = () => {
-    if(filterValue === 'Incomplete'){
-        const value = tasks.filter((task) => task.completed == false)
-        setFilteredTask([...value])
-    } else if (filterValue === 'Complete'){
-        const value = tasks.filter((task) => task.completed == true)
-        setFilteredTask([...value])
-    } else {
-        setFilteredTask([...tasks])
+    //Filter task logic
+    const setFilteredValue = () => {
+        if (filterValue === 'Incomplete') {
+            let value = tasks.filter((task) => task.completed == false)
+            if (sortType === 'ASC') {
+                value = value.sort((first, second) => (first.id > second.id ? 1 : second.id > first.id ? -1 : 0))
+            } else {
+                value = value.sort((first, second) => (first.id > second.id ? -1 : second.id > first.id ? 1 : 0))
+            }
+            setFilteredTask([...value])
+        } else if (filterValue === 'Complete') {
+            let value = tasks.filter((task) => task.completed == true)
+            if (sortType === 'ASC') {
+                value = value.sort((first, second) => (first.id > second.id ? 1 : second.id > first.id ? -1 : 0))
+            } else {
+                value = value.sort((first, second) => (first.id > second.id ? -1 : second.id > first.id ? 1 : 0))
+            }
+            setFilteredTask([...value])
+        } else {
+            let value = [...tasks]
+            if (sortType === 'ASC') {
+                value = value.sort((first, second) => (first.id > second.id ? 1 : second.id > first.id ? -1 : 0))
+            } else {
+                value = value.sort((first, second) => (first.id > second.id ? -1 : second.id > first.id ? 1 : 0))
+            }
+            setFilteredTask([...value])
+        }
     }
-  }
 
-  useEffect(() => {
-    localStorage.setItem('data',JSON.stringify(tasks))
-    setFilteredValue()
-  },[tasks])
+    useEffect(() => {
+        localStorage.setItem('data', JSON.stringify(tasks))
+        setFilteredValue()
+    }, [tasks])
 
-  useEffect(() => {
-    setFilteredValue()
-  },[filterValue])
+    useEffect(() => {
+        setFilteredValue()
+    }, [filterValue])
 
-  return (
-    <div>
-      <h1>Task Management</h1>
-      <TaskForm addTask={addTask} />
-      <div>
-        <button onClick={() => {setFilterValue('All')}}>All</button>
-        <button onClick={() => {setFilterValue('Incomplete')}}>Incomplete</button>
-        <button onClick={() => {setFilterValue('Complete')}}>Completed</button>
-      </div>
-      <TaskList tasks={filteredTask} updateTask={updateTask} deleteTask={deleteTask} />
-    </div>
-  );
+    useEffect(() => {
+        setFilteredValue()
+    }, [sortType])
+
+    return (
+        <div>
+            <h1>Task Management</h1>
+            <TaskForm addTask={addTask} />
+            <div>
+                <button onClick={() => { setFilterValue('All') }}>All</button>
+                <button onClick={() => { setFilterValue('Incomplete') }}>Incomplete</button>
+                <button onClick={() => { setFilterValue('Complete') }}>Completed</button>
+            </div>
+            <div>
+                <label> Sort by Create time
+                    <select value={sortType} onChange={(e) => setSortType(e.target.value)}>
+                        <option value="ASC">Ascending</option>
+                        <option value="DES">Descending</option>
+                    </select>
+                </label>
+            </div>
+            <TaskList tasks={filteredTask} updateTask={updateTask} deleteTask={deleteTask} />
+        </div>
+    );
 }
 
 export default HomePage;
